1582d7826bfd9b910e7e454e6a5c137d1c6d3820
altunfatih <50260139+altunfatih@users.noreply.github.com>
2020-09-27 09:58:55 +0300

    Add files via upload

---
+++ 034.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        a dizisi içerisinde teklerı başa çiftleri sona atayan program
+        2. dizi kullanmayınız.
+        int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
+         */
+
+        int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
+        int size, po, pe;
+        size = 10;
+        po = 0;
+        pe = 0;
+
+        for (int i = 0; i < size; i++)
+            if (a[i] % 2 != 0) {
+                pe = a[po];
+                a[po] = a[i];
+                po++;
+                a[i] = pe;
+            }
+
+        for (int i = 0; i < size; i++)
+            System.out.print(a[i] + " ");
+    }
+}
